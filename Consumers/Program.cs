33ef2368fcf07116730fcd07ceca319361b962ff
csmcorporatama@gmail.com <indorent10>
2023-06-30 17:44:58 +0700

    init

---
+++ Consumers/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:29092",
+    GroupId = "my-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+{
+    consumer.Subscribe("my-topic");
+
+    while (true)
+    {
+        try
+        {
+            var consumeResult = consumer.Consume();
+            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Value}");
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Error occured: {e.Error.Reason}");
+        }
+    }
+}
---
+++ Producers/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ProducerConfig { BootstrapServers = "localhost:29092" };
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        var dr = await producer.ProduceAsync("my-topic", new Message<Null, string> { Value = "hello kocik" });
+        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+    }
+}
